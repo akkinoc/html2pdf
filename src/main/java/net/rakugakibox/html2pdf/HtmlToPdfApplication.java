25ea2f3336977588fff092e8761a65e896b3be52
Akihiro Kondo <akihyro@gmail.com>
2015-11-10 13:34:21 +0900

    Implement main method.

--- src/main/java/net/rakugakibox/html2pdf/HtmlToPdfApplication.java
+++ src/main/java/net/rakugakibox/html2pdf/HtmlToPdfApplication.java
@@ -1,8 +1,23 @@
 package net.rakugakibox.html2pdf;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
 public class HtmlToPdfApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        String html = args[0];
+        String pdf = args[1];
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocument(html);
+        renderer.layout();
+        try (OutputStream out = new FileOutputStream(pdf)) {
+            renderer.createPDF(out);
+        }
+
     }
 
 }
